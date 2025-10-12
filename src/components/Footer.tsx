1464c41adebbcd42ac548de632346e70010a3ab5
douvy <itsdouvy@gmail.com>
2025-10-12 02:48:27 -0400

    Change footer bg

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,10 @@
 export default function Footer() {
   return (
-    <footer className="bg-primary-blue py-4 md:py-6">
+    <footer className="bg-[#15171a] border-t border-[#1b2028] py-4 md:py-6">
       <div className="container mx-auto px-4 text-center">
         <a
           href="https://github.com/douvy/remilia-stats"
-          className="text-[#f1f2f4] transition-colors border-b border-[#5175d1] pb-[1px] hover:border-[#f1f2f4]"
+          className="text-[#b8bdc7] hover:text-white transition-colors border-b border-[#2d323b] pb-[1px] hover:border-[#b8bdc7]"
           target="_blank"
           rel="noopener noreferrer"
         >
